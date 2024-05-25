310a9f57b73bc4004f3da51e62fc378d8219e6f7
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-05-25 11:28:18 +0100

    static config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
+// see https://kit.svelte.dev/docs/adapter-static 
+// for gh-pages hosting
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+    kit: {
+		paths: {
+			base: '/sveltekit-gh-pages',
+		},
+		adapter: adapter({
+			fallback: '404.html',
+
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
