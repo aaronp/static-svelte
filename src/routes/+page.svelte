d53e6fe9241a059b01c6ba5466f67ade803bd631
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-05-25 11:36:10 +0100

    fix for link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-See the code <a href="https://github.com/aaronp/static-svelte"></a> here</a>
+See the code <a href="https://github.com/aaronp/static-svelte">here</a>
